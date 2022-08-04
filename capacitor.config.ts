1bbb0d39e2dc3bf300328c469a06da7d628f5b1c
unknown <AMTuring@RES06-P040.mpf.gov.ar>
2022-08-04 08:11:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MPA_TEST',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
